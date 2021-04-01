6abefb0b949f051fff4e4cecb6e0427347bcc71d
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-04-01 19:29:32 +0800

    confetti

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import Confetti from "react-confetti";
+
+const App = () => {
+    return (
+        <div className="flex items-center justify-center w-screen h-screen">
+            <Confetti />
+        </div>
+    );
+};
+
+export default App;
